09e8453ab9d07b4fea96cbc009a8951bbc4cb3b0
AdityaNawandar <nawandaraditya60@gmail.com>
2024-12-09 12:55:18 +0530

    Resume path correction

--- lib/utils/file_download.dart
+++ lib/utils/file_download.dart
@@ -1,26 +1,18 @@
 // import 'dart:typed_data';
 import 'package:file_selector/file_selector.dart';
 import 'package:flutter/services.dart'; // To load the asset file
-import 'package:flutter/foundation.dart' show kIsWeb; // For detecting web
+import 'package:portfolio/constants/constants.dart';
 
 class FileDownloader {
+  ///
   static Future<void> downloadFileToUserComputer() async {
+    ///
     String assetPath;
-
-    // Detect platform and adjust the path accordingly
-    if (kIsWeb) {
-      assetPath =
-          'files/resume_aditya_nawandar.pdf'; // Web doesn't need 'assets/'
-    } else {
-      assetPath =
-          'assets/files/resume_aditya_nawandar.pdf'; // Mobile needs 'assets/'
-    }
-
+    assetPath = '${kstrResumeFileDirectoryPath}resume_aditya_nawandar.pdf';
     const String mimeType = 'application/pdf';
     // Load the file from assets
     ByteData byteData = await rootBundle.load(assetPath);
     Uint8List fileData = byteData.buffer.asUint8List();
-
     // Ask the user for a location to save the file
     final FileSaveLocation? result =
         await getSaveLocation(suggestedName: 'resume_aditya_nawandar.pdf');
@@ -28,15 +20,15 @@ class FileDownloader {
       // User canceled the operation
       return;
     }
-
     // Create a file object from the file data
     final XFile pdfFile = XFile.fromData(
       fileData,
       mimeType: mimeType,
       name: 'resume_aditya_nawandar.pdf',
     );
-
     // Save the file to the chosen path
     await pdfFile.saveTo(result.path);
   }
+
+  ///
 }
